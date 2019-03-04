2d08160c97f6e9d43241d1a4ef1edcf6c9b6e4b5
nehringtb <thomasnehring@outlook.com>
2019-03-03 18:30:20 -0700

    Corrected naming of app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_20_211636) do
+ActiveRecord::Schema.define(version: 2019_03_02_220328) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -19,11 +19,12 @@ ActiveRecord::Schema.define(version: 2019_01_20_211636) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "recepies", force: :cascade do |t|
+  create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "chef_id"
   end
 
 end
---
+++ test/models/recipe_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+	def setup
+		@chef = Chef.create(chefname: "Thomas", email: "thomas@something.com")
+		@recipe = @chef.recipes.build(name: 'vegetable soup', description: "Great food")
+	end	
+
+	test "recipe should be valid" do
+		assert @recipe.valid?
+	end
+
+	test "name should be present" do
+		@recipe.name = " "
+		assert_not @recipe.valid?
+	end
+
+	test "description should be present" do
+		@recipe.description = " "
+		assert_not @recipe.valid?
+	end
+	
+	test 'description shouldnt be less than 5 chars' do
+		@recipe.description = "a" * 3
+		assert_not @recipe.valid?
+	end
+
+	test 'description shouldnt be more thant 500 chars' do
+		@recipe.description = "a" * 501
+		assert_not @recipe.valid?
+	end
+
+end
