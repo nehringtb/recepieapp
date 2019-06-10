b596919f058c22d03928e41dbed8f9e6dbf277d8
nehringtb <thomasnehring@outlook.com>
2019-06-10 16:04:10 -0600

    Adds create chef functionality.

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,7 +1,27 @@
 class ChefsController < ApplicationController
 
 	def new
+		@chef = Chef.new	
+	end
+
+	def create 
+		@chef = Chef.new(chef_params)
+		if @chef.save
+			flash[:success] = "Welcome to my recipes app"
+			redirect_to chef_path(@chef)
+		else
+			render 'chefs/new'
+		end
+	end
 	
+	def show
+
+	end
+
+	private
+
+	def chef_params
+		params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
 	end
 
 end
